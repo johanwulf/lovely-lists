5ec3001d68accd8172a6e0db4d48e69dda0f835d
johanwulf <johanwulf98@gmail.com>
2023-09-18 19:14:19 +0200

    feat: initial sso auth implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -34,13 +35,17 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <html lang="en" suppressHydrationWarning>
                 <head />
                 <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-                    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-                        <div className="relative flex min-h-screen flex-col">
-                            <SiteHeader />
-                            <div className="flex-1">{children}</div>
-                        </div>
-                        <TailwindIndicator />
-                    </ThemeProvider>
+                    <UserProvider>
+                        <body>
+                            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+                                <div className="relative flex min-h-screen flex-col">
+                                    <SiteHeader />
+                                    <div className="flex-1">{children}</div>
+                                </div>
+                                <TailwindIndicator />
+                            </ThemeProvider>
+                        </body>
+                    </UserProvider>
                 </body>
             </html>
         </>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
+"use client";
+
 import CreateList from "@/components/create-list";
 import Lists from "@/components/lists";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 export default function IndexPage() {
+    const { user, error, isLoading } = useUser();
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>{error.message}</div>;
+
     return (
         <section className="container max-w-lg grid items-center gap-6 pb-8 pt-6 md:py-10">
-            <Lists />
-            <CreateList />
+            {user ? (
+                <>
+                    {user.name}
+                    {user.email}
+                    <a href="/api/auth/logout">Logout</a>
+                    <Lists />
+                    <CreateList />
+                </>
+            ) : (
+                <a href="/api/auth/login">Login</a>
+            )}
         </section>
     );
 }
